3c74e52385e9a20f51f58c8f699e93c9b51d3e07
ravi <123pravikumar@gmail.com>
2026-05-30 21:47:23 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+OLLAMA_BASE_URL = "http://localhost:11434"
+LLM_MODEL = "llama2:latest"
+
+DEFAULT_SQL_PATH = "./data/sales.db"
+
+DEFAULT_EXCEL_FILES = {
+    "employees": "./data/employees.xlsx"
+}
+
+DEFAULT_CSV_FILES = {
+    "web_analytics": "./data/web_analytics.csv"
+}
